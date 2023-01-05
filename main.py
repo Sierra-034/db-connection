0cd4ca36c4c29072b4d217f7d23c6f2d05e8b579
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-01-05 00:38:44 -0600

    03: Crear tabla

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pymysql
 
+DROP_TABLE_USERS = "DROP TABLE IF EXISTS users"
+
+USERS_TABLE = """CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)"""
+
 if __name__ == '__main__':
     try:
-        connnect = pymysql.Connect(
+        connect = pymysql.Connect(
             host='127.0.0.1',
             port=3306,
             user='root',
             passwd='sm-pass',
             db='pythondb',
         ) 
-
+        cursor = connect.cursor()
+        cursor.execute(DROP_TABLE_USERS)
+        cursor.execute(USERS_TABLE)
         print('Conexión realizada de forma exitosa!')
     except pymysql.err.OperationalError as err:
         print('No fué posible realizar la conexión')
